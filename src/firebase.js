7d5c61ae427ddaf1d56aaf6cdb63811c5dfc1385
Shin-sibainu <shincode0712@gmail.com>
2021-09-02 14:18:17 +0900

    router settings

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQD5y6m0tBBgQo099RrN2KgbjMlyBXXZg",
+  authDomain: "slack-clone-2-14677.firebaseapp.com",
+  projectId: "slack-clone-2-14677",
+  storageBucket: "slack-clone-2-14677.appspot.com",
+  messagingSenderId: "297415030355",
+  appId: "1:297415030355:web:1d1e2abee832384d7838a6",
+};
