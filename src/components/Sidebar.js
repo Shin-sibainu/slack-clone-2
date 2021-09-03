77071de58ef2385ed0bc30acffa0ab51edfb35ad
Shin-sibainu <shincode0712@gmail.com>
2021-09-03 11:06:30 +0900

    sidebar is almost completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,20 @@
-import { FiberManualRecord } from "@material-ui/icons";
+import {
+  Add,
+  Apps,
+  BookmarkBorder,
+  Drafts,
+  ExpandLess,
+  ExpandMore,
+  FiberManualRecord,
+  FileCopy,
+  Inbox,
+  PeopleAlt,
+} from "@material-ui/icons";
 import CreateIcon from "@material-ui/icons/Create";
 import React from "react";
 import styled from "styled-components";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
 
 function Sidebar() {
   return (
@@ -13,9 +26,22 @@ function Sidebar() {
             <FiberManualRecord />
             Shin Code
           </h3>
-          <CreateIcon />
         </SidebarInfo>
+        <CreateIcon />
       </SidebarHeader>
+
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={Inbox} title="Mentions & reactions" />
+      <SidebarOption Icon={Drafts} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorder} title="Channel browser" />
+      <SidebarOption Icon={PeopleAlt} title="People & user groups" />
+      <SidebarOption Icon={Apps} title="Apps" />
+      <SidebarOption Icon={FileCopy} title="File browser" />
+      <SidebarOption Icon={ExpandLess} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMore} title="Channel" />
+      <hr />
+      <SidebarOption Icon={Add} addChannelOption title="Add Channel" />
     </SidebarContainer>
   );
 }
@@ -29,8 +55,48 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
-const SidebarHeader = styled.div``;
+const SidebarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  padding: 13px;
 
-const SidebarInfo = styled.div``;
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #49274b;
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
+`;
+
+const SidebarInfo = styled.div`
+  flex-grow: 1;
+
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
+  }
+`;
