9f1c829590d36eb2a77620acc17c852a07fb5fa8
Shin-sibainu <shincode0712@gmail.com>
2021-09-03 17:11:02 +0900

    firebase and front connected

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAQD5y6m0tBBgQo099RrN2KgbjMlyBXXZg",
   authDomain: "slack-clone-2-14677.firebaseapp.com",
@@ -6,3 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "297415030355",
   appId: "1:297415030355:web:1d1e2abee832384d7838a6",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, db, provider };
